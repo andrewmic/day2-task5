cebbb3e05db261bd8d90b8b57e6d4a4ba5dcc3f0
sostakas <ernestas.sostakas@gmail.com>
2018-03-05 23:35:26 +0200

    First commit

---
+++ InterestCalculation/src/lt/swedbank/interestcalculator/CompoundInterestCalculator.java
@@ -0,0 +1,128 @@
+package lt.swedbank.interestcalculator;
+import java.util.Scanner;
+
+/**
+ * Created by p998feq on 2018.03.05.
+ */
+
+public class CompoundInterestCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float originalAmount;
+
+        do {
+            System.out.print("Amount: ");
+            originalAmount = getFloat();
+
+            if(originalAmount <= 0)
+                System.out.println("Invalid input! Try again!");
+        }while (originalAmount <= 0);
+
+        int periodLength;
+        do {
+            System.out.print("Period length (years): ");
+            periodLength = getInt();
+            if(periodLength <= 0)
+                System.out.println("Invalid input! Try again!");
+
+
+        }while (periodLength <= 0);
+
+        char compoundFrequency;
+        do {
+            System.out.print("Compound  frequency: ");
+            compoundFrequency = scanner.next().charAt(0);
+
+            if(compoundFrequency == 'D' || compoundFrequency == 'W' || compoundFrequency == 'M' || compoundFrequency == 'Q' || compoundFrequency == 'H' ||
+                    compoundFrequency == 'Y' )
+                break;
+            else
+                System.out.println("Invalid input! Try again!");
+        }while (true);
+
+        int interestRateCount = -1;
+        double[] interestRateArray;
+        interestRateArray = new double[10];     // kaip saugot cikle nezinant kiek galima interest rate ivest?
+        float interestRate;
+        do {
+            System.out.print("Interest rate (%): ");
+            interestRate = getFloat();
+            ++interestRateCount;
+            interestRateArray[interestRateCount] = interestRate;
+            if(interestRate == 0)
+                break;
+            else if(interestRate <= 0 || interestRate > 100){
+                System.out.println("Invalid input! Try again!");
+                --interestRateCount;
+            }
+        }while (true);
+
+        double[][] tempIntermediateInterestAmounts;
+        tempIntermediateInterestAmounts = new double[interestRateCount][(int)periodLength * getCompoundFrequency(compoundFrequency)];
+        double[][] intermediateInterestAmounts;
+        intermediateInterestAmounts = new double[interestRateCount][(int)periodLength * getCompoundFrequency(compoundFrequency)];
+
+        int pos;
+        for (int j = 0; j < interestRateCount; j++){
+            pos = 0;
+            for (int i = 1; i <= periodLength; i++) {
+                tempIntermediateInterestAmounts[j][pos] = getInterestAmount(originalAmount, interestRateArray[j], i, getCompoundFrequency(compoundFrequency));
+
+                if (pos > 0)
+                    intermediateInterestAmounts[j][pos] = tempIntermediateInterestAmounts[j][pos] - tempIntermediateInterestAmounts[j][pos - 1];
+                else
+                    intermediateInterestAmounts[j][pos] = tempIntermediateInterestAmounts[j][pos];
+                pos++;
+            }
+        }
+
+        for (double[] row : intermediateInterestAmounts) {
+            for (double number : row) {
+                System.out.print(String.format("%.2f", number) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static double getInterestAmount(double originalAmount, double interestRate, double periodLength, int compoundFrequency) {
+        return originalAmount * Math.pow((1 + (interestRate / 100) / compoundFrequency), periodLength * compoundFrequency) - originalAmount;
+    }
+
+    private static int getCompoundFrequency(char compoundFrequencyString) {
+        switch (compoundFrequencyString) {
+            case 'D':
+                return 365;
+            case 'W':
+                return 52;
+            case 'M':
+                return 12;
+            case 'Q':
+                return 4;
+            case 'H':
+                return 2;
+            case 'Y':
+                return 1;
+            default:
+                return 1;
+        }
+    }
+    private static int getInt() {
+        while(true){
+            try {
+                return Integer.parseInt(new Scanner(System.in).next());
+            } catch(NumberFormatException ne) {
+                System.out.println("Invalid input! Try again!");
+            }
+        }
+    }
+
+    private static Float getFloat() {
+        while(true){
+            try {
+                return Float.parseFloat(new Scanner(System.in).next());
+            } catch(NumberFormatException ne) {
+                System.out.println("Invalid input! Try again!");
+            }
+        }
+    }
+}
